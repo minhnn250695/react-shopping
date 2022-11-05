34faa9133eecba73600bb6efb401f6cb094c04a1
Nguyen Nhat Minh <minh.nn250695@gmail.com>
2022-11-05 21:53:21 +0700

    Init axios client and categories get all

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import categoryApi from './api/categoriesAPI';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {}, []);
+  const fetcheCategories = async () => {
+    const categories = await categoryApi.getAll();
+    console.log(categories);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +18,9 @@ function App() {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <button type="button" onClick={fetcheCategories}>
+          Get categories
+        </button>
       </header>
     </div>
   );
